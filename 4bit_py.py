267b5c68d8f9549bf4a078ec38d9d18e89ca2d13
Hitler-jon <bakhodirboymuradov@gmail.com>
2026-05-07 14:03:11 +0900

    Add files via upload

---
+++ 4bit_py.py
@@ -0,0 +1,13 @@
+#math_Bakhodir_202122013
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10%3)	
+print(10//3)	#floor division 3
+print(10**3)	#exp10^3
+print(10==3)
+print(10!=3)
+print(10<3 or 3<10)
+print(10<3 and 3<10)
+print(not 10<3)
